7c1fa4598761fc438e7f434ca1cbcac62adc810c
AliceAAtalla <aliceatalla36@gmail.com>
2020-10-09 09:58:17 -0300

    Updates

---
+++ precache-manifest.61060f3895d6460c40b55dd51b1c3e2a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee21cdbcd31e77f152122cde955006b2",
+    "url": "/CuteFlix/index.html"
+  },
+  {
+    "revision": "a50a9d59a9a9e977ab1e",
+    "url": "/CuteFlix/static/js/2.7e8229a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CuteFlix/static/js/2.7e8229a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ed8e5c159d9e2a5b079",
+    "url": "/CuteFlix/static/js/main.cac31a91.chunk.js"
+  },
+  {
+    "revision": "e9adff8ede9a2d3c1233",
+    "url": "/CuteFlix/static/js/runtime-main.d21b9deb.js"
+  },
+  {
+    "revision": "79157aff9de8f3cc3415af22ed32f041",
+    "url": "/CuteFlix/static/media/Channel.79157aff.jpg"
+  },
+  {
+    "revision": "3ebcf4914e85ab51a698afde56ed7123",
+    "url": "/CuteFlix/static/media/Chorrinho1.3ebcf491.jpg"
+  },
+  {
+    "revision": "8c52693538ba054e7660b5baa7b83cab",
+    "url": "/CuteFlix/static/media/CuteFlix.8c526935.png"
+  },
+  {
+    "revision": "27d97c634e81cd1f67d138867e5f21a5",
+    "url": "/CuteFlix/static/media/arrow.27d97c63.svg"
+  }
+]);
